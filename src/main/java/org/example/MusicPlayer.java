7a1c91332d48644f3d1b8d100d454dfd52106e0a
Julia Artemieva <jul.art1302@gmail.com>
2021-04-13 09:59:20 +0300

    add setter and getter for name and volume

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -6,6 +6,22 @@ public class MusicPlayer {
     private String name;
     private int volume;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
