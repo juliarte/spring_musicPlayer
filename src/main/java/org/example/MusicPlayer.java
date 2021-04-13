1a09bf2fff3b85083994680dab8f09c07b61e26b
Julia Artemieva <jul.art1302@gmail.com>
2021-04-13 09:58:10 +0300

    private var name and volume was added

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,6 +3,9 @@ package org.example;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
